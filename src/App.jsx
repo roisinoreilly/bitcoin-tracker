63acd64a0f7d20961f87c87a30fde9d8f538d570
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-06-06 18:07:25 +0100

    move files to add .css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Tracker from "./pages/Tracker";
-import Converter from "./pages/Converter";
-
+import Tracker from "./pages/Tracker/Tracker";
+import Converter from "./pages/Converter/Converter";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
