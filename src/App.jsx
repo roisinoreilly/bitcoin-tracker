e8f4e399fcf8490fe316cc8da166ed9e4d8bab3d
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-06-06 18:02:44 +0100

    add Converter view

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tracker from "./pages/Tracker";
+import Converter from "./pages/Converter";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Tracker />} />
+          <Route path="/converter" element={<Converter />} />
         </Routes>
       </BrowserRouter>
     </>
