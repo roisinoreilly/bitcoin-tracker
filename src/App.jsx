2a5d2b0a4866c95029270cd1e10bb8a912fc9d7c
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-06-06 18:20:51 +0100

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tracker from "./pages/Tracker/Tracker";
 import Converter from "./pages/Converter/Converter";
 import Nav from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
       <Nav/>
         <Routes>
           <Route path="/" element={<Tracker />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import "./Header.css"
+
+const Header = () => {
+    return <h1>Bitcoin Tracker</h1>
+};
+
+export default Header;
