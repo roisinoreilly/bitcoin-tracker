54a5488cf5b4a96288736fec8d8b67403865b907
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-06-06 18:15:23 +0100

    add basic CurrencyCard styling

--- src/components/CurrencyCard/CurrencyCard.jsx
+++ src/components/CurrencyCard/CurrencyCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import "./CurrencyCard.css"
 
 const CurrencyCard = ({ currencyCode, exchangeRates }) => {
     return (
-        <div>
-            <div>
+        <section className="currency-card">
+            <div className="currency-header">
                 <h2>{currencyCode}</h2>
             </div>
-            <div>
+            <div className="currency-details">
                 <p>
                     Last: {exchangeRates.symbol} {exchangeRates.last}
                 </p>
             </div>
-        </div>
+        </section>
     );
   };
   
